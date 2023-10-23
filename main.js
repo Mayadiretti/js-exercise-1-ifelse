ac0db5f7bee2bc6d3d96edcb98bd54f8312c61a4
maya diretti <maya.diretti@icloud.com>
2023-10-23 19:52:29 +0200

    feat / mancia

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-let voti = prompt('che voti avete preso voi 3?');
-let num1 = 8;
-let num2 = 7;
-let num3 = 5;
+let mancia = prompt('quanto è il conto e la percentuale della mancia?')
+let number1 = 200; 
+let number2= 15;
 
-let result = (num1 + num2 + num3) / 3;
-alert (result) 
+let result = (number1 * number2) / 100 ;
+alert(result)
