4c474fd061abdde4fcfcbfbd96ddb604a03a9242
maya diretti <maya.diretti@icloud.com>
2023-10-23 19:30:11 +0200

    feat / voti

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-let temperatura = prompt('quanti sono 64 Fahrenheit?')
-let Fahrenheit = 64;
-Celsius= 32;
- let result= Fahrenheit / Celsius;
- alert (result)
+let voti = prompt('che voti avete preso voi 3?');
+let num1 = 8;
+let num2 = 7;
+let num3 = 5;
+
+let result = (num1 + num2 + num3) / 3;
+alert (result) 
