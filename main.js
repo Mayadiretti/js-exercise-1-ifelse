ad2d107456e13eafb4095689143ea6196c763b55
maya diretti <maya.diretti@icloud.com>
2023-10-23 23:00:48 +0200

    feat / cinema

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-let mancia = prompt('quanto è il conto e la percentuale della mancia?')
-let number1 = 200; 
-let number2= 15;
-
-let result = (number1 * number2) / 100 ;
-alert(result)
+let cinema = prompt('quanti anni hai?')
+let età = 15;
+if(età < 13){
+    alert('solo film con classificazione PG')
+}
+if(età > 17){
+    alert('possono vedere tutti i film')
+}else{
+    alert('possono vedere film con PG e PG-13')
+}
